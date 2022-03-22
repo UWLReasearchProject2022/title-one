d31e06f30371c330484f530a9a5ba587df187a8d
Ed Fisk <Ed.Fisk@arup.com>
2022-03-22 12:27:25 +0000

    Added base url

--- client/src/components/organisms/Header/Header.tsx
+++ client/src/components/organisms/Header/Header.tsx
@@ -11,12 +11,16 @@ import { IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
 import AccountIcon from "@mui/icons-material/AccountCircleSharp";
 import BasketIcon from "@mui/icons-material/ShoppingBasketSharp";
+import { BASE_URL } from "utils/config";
 
 export const Header: React.FunctionComponent = () => {
   return (
     <Container>
       <Link to="/">
-        <Logo src="logos/title-one-full-logo.svg" alt="title-one-full-logo" />
+        <Logo
+          src={`${BASE_URL}logos/title-one-full-logo.svg`}
+          alt="title-one-full-logo"
+        />
       </Link>
       <LargeSpacer />
       {["Xbox", "PlayStation", "Nintendo", "PC Gaming"].map((text) => (
