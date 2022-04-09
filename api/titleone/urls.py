c1005e9bde7f50380134d369d344c204e2ec870d
YasserAbubakar <81751770+YasserAbubakar@users.noreply.github.com>
2022-04-09 16:25:34 +0100

    file and folder restructure

--- api/titleone/urls.py
+++ api/titleone/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from api.views import getProducts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path("products/", getProducts)
 ]
