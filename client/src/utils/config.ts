6fef9122d77edab20ae764974ace217269ca4c85
James Grogan <james.grogan2@gmail.com>
2022-05-13 22:03:52 +0100

    fix logo route

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.REACT_APP_ENV === "production"
-    ? "https://titleone.co.uk/"
+    ? "https://titleone.co.uk"
     : `http://localhost:${process.env.PORT ? process.env.PORT : 3000}/`;
